449a70605c184f2407971379470933d888b1df68
dkshs <88355080+dkshs@users.noreply.github.com>
2024-08-15 16:34:15 -0300

    fix: update pytest style to match update ruff rules

--- template/{{dkcutter.projectSlug}}/tests/test_admin.py
+++ template/{{dkcutter.projectSlug}}/tests/test_admin.py
@@ -4,13 +4,13 @@ import pytest
 from django.urls import reverse
 
 
-@pytest.mark.django_db()
+@pytest.mark.django_db
 def test_admin_page_accessible_by_admin(admin_client):
     response = admin_client.get(reverse("admin:index"))
     assert response.status_code == HTTPStatus.OK
 
 
-@pytest.mark.django_db()
+@pytest.mark.django_db
 def test_admin_page_not_accessible_by_normal_user(client):
     response = client.get(reverse("admin:index"))
     assert response.status_code == HTTPStatus.FOUND
