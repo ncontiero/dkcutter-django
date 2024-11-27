f4aa4b3f18846c45e8c2ce3b7b7620ccd3e11c39
dkshs <88355080+dkshs@users.noreply.github.com>
2024-11-27 19:06:16 -0300

    feat: add support for secure redis (TLS support)

--- template/{{dkcutter.projectSlug}}/config/settings/base.py
+++ template/{{dkcutter.projectSlug}}/config/settings/base.py
@@ -3,6 +3,9 @@
 Base settings to build other settings files upon.
 """
 
+{% if dkcutter.useCelery -%}
+import ssl
+{%- endif %}
 from pathlib import Path
 
 import dj_database_url
@@ -205,6 +208,7 @@ LOGGING = {
 }
 
 REDIS_URL = config("REDIS_URL", default="redis://redis:6379/0")
+REDIS_SSL = REDIS_URL.startswith("rediss://")
 
 {% if dkcutter.useCelery %}
 # Celery
@@ -214,8 +218,12 @@ if USE_TZ:
     CELERY_TIMEZONE = TIME_ZONE
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-broker_url
 CELERY_BROKER_URL = REDIS_URL
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#redis-backend-use-ssl
+CELERY_BROKER_USE_SSL = {"ssl_cert_reqs": ssl.CERT_NONE} if REDIS_SSL else None
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-result_backend
 CELERY_RESULT_BACKEND = REDIS_URL
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#redis-backend-use-ssl
+CELERY_REDIS_BACKEND_USE_SSL = CELERY_BROKER_USE_SSL
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#result-extended
 CELERY_RESULT_EXTENDED = True
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#result-backend-always-retry
