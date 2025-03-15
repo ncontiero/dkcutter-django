6051fa1986e287f69c4f801c9447699259d3f6ba
dkshs <88355080+dkshs@users.noreply.github.com>
2025-03-15 13:54:00 -0300

    refactor: fix ruff errors

--- template/{{dkcutter.projectSlug}}/config/wsgi.py
+++ template/{{dkcutter.projectSlug}}/config/wsgi.py
@@ -1,4 +1,3 @@
-# ruff: noqa
 """
 WSGI config for {{ dkcutter.projectName }} project.
 
--- template/{{dkcutter.projectSlug}}/manage.py
+++ template/{{dkcutter.projectSlug}}/manage.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python
-# ruff: noqa
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 from pathlib import Path
 
 
 def main():
+    """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
     try:
         from django.core.management import execute_from_command_line
-    except ImportError:
-        try:
-            import django
-        except ImportError as e:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            ) from e
-        raise
+    except ImportError as exc:
+        raise ImportError(  # noqa: TRY003
+            "Couldn't import Django. Are you sure it's installed and "  # noqa: EM101
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?",
+        ) from exc
 
     current_path = Path(__file__).parent.resolve()
     sys.path.append(str(current_path / "{{ dkcutter.projectSlug }}"))
+
     execute_from_command_line(sys.argv)
 
 
