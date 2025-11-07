42800de88eaad8c241a074d39775a023bb251439
Nicolas Contiero <git@ncontiero.com>
2025-11-07 12:28:35 -0300

    feat: add theme toggle and header UI enhancements

--- template/{{dkcutter.projectSlug}}/{{dkcutter.projectSlug}}/static/js/project.js
+++ template/{{dkcutter.projectSlug}}/{{dkcutter.projectSlug}}/static/js/project.js
@@ -1,1 +1,13 @@
+import { initThemeToggle } from "./theme-toggle.js";
+
+const initializeApp = () => {
+  setTimeout(() => {
+    document.documentElement.classList.replace("opacity-0", "opacity-100");
+  }, 500);
+
+  initThemeToggle();
+};
+
+document.addEventListener("DOMContentLoaded", initializeApp);
+
 /* Project specific Javascript goes here. */
